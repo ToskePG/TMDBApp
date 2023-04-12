afe538247f7ef7577b0cae442114801f146e9d6c
ToskePG <danilo.tosic.pmf@gmail.com>
2023-04-12 11:06:25 +0200

    Creates layout for movie tabs on home screen.

---
+++ app/src/main/java/com/example/tmdbapp/presentation/home/MovieTabComponent.kt
@@ -0,0 +1,11 @@
+package com.example.tmdbapp.presentation.home
+
+import androidx.compose.runtime.Composable
+import com.example.tmdbapp.domain.model.Movie
+
+@Composable
+fun MovieTab(
+    movies: List<Movie>
+){
+    
+}
