1e3236e0bdb944369157737d035f158f3539dcfe
ToskePG <danilo.tosic.pmf@gmail.com>
2023-04-12 02:01:04 +0200

    Refactors movie poster to be inside of box

--- app/src/main/java/com/example/tmdbapp/presentation/home/MoviePoster.kt
+++ app/src/main/java/com/example/tmdbapp/presentation/home/MoviePoster.kt
@@ -1,9 +1,13 @@
 package com.example.tmdbapp.presentation.home
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tmdbapp.ui.theme.TMDBAppTheme
 
 @Composable
@@ -11,9 +15,13 @@ fun MoviePoster(
     // Before API calls, later change to URL
     movie_Poster: Int
 ){
-    Image(
-        painter = painterResource(id = movie_Poster),
-        contentDescription = null)
+    Box (modifier = Modifier
+        .padding(all = 12.dp)
+    ){
+        Image(
+            painter = painterResource(id = movie_Poster),
+            contentDescription = null)
+    }
 }
 
 @Preview
