03cc8cdfee22a2fa655be95732a729763bd8a9c5
ToskePG <danilo.tosic.pmf@gmail.com>
2023-04-18 12:23:11 +0200

    Creates tab menu

--- app/src/main/java/com/example/tmdbapp/presentation/home/MovieTabComponent.kt
+++ app/src/main/java/com/example/tmdbapp/presentation/home/MovieTabComponent.kt
@@ -1,10 +1,21 @@
 package com.example.tmdbapp.presentation.home
 
-import androidx.compose.material.Tab
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.TabRow
-import androidx.compose.material.Text
+import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color.Companion.LightGray
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.material.Tab
+import androidx.compose.material.Text
+import androidx.compose.ui.unit.sp
 import com.example.tmdbapp.ui.theme.TMDBAppTheme
 
 @Composable
@@ -12,20 +23,32 @@ fun MovieTabComponent(
     selectedTabIndex: Int,
     onSelectedTab: (TabPage) -> Unit
 ){
-    TabRow(selectedTabIndex = selectedTabIndex) {
-        TabPage.values().forEachIndexed{ index, tabPage ->
-            Tab(
-                selected = index == selectedTabIndex,
-                onClick = {onSelectedTab(tabPage)},
-                text = {Text(text = tabPage.name)}
-                )
+    TabRow(selectedTabIndex = selectedTabIndex,
+        indicator = {tabPositions ->
+            Box(
+                modifier = Modifier
+                    .tabIndicatorOffset(tabPositions[selectedTabIndex])
+                    .height(4.dp)
+                    .padding(horizontal = 28.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(color = LightGray)
+            )
         }
+        ) {
+            TabPage.values().forEachIndexed { index, tabs ->
+                Tab(
+                    selected = index == selectedTabIndex,
+                    onClick = {onSelectedTab(tabs)},
+                    text = {Text(text = tabs.name, maxLines = 1, fontSize = 14.sp)}
+                )
+            }
     }
 }
 enum class TabPage{
     NOW_PLAYING,
-    POPULAR,
-    UPCOMING
+    UPCOMING,
+    TOP_RATED,
+    POPULAR
 }
 
 @Preview
