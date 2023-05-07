1035691787732e62226929b79852fab7aea734d3
ToskePG <danilo.tosic.pmf@gmail.com>
2023-05-07 19:18:53 +0200

    Creates MovieDto for transfering objects in the app

---
+++ app/src/main/java/com/example/tmdbapp/data/remote/dto/MovieDto.kt
@@ -0,0 +1,17 @@
+package com.example.tmdbapp.data.remote.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieDto(
+    val id: Int? = null,
+    val title: String? = null,
+    @SerializedName("poster_path")
+    val moviePoster: String? = null,
+    @SerializedName("vote_average")
+    val averageVote: Float? = null,
+    val genres: List<Int>? = null,
+    @SerializedName("release_date")
+    val date: String? = null,
+    val runtime: Int? = null,
+    val overview: String? = null
+)
