b8799adbd3fe6f047534c4900f603e15937a1f18
ToskePG <danilo.tosic.pmf@gmail.com>
2023-05-07 20:05:29 +0200

    Works on displaying network data inside screen

--- app/src/main/java/com/example/tmdbapp/presentation/home/MovieTabComponent.kt
+++ app/src/main/java/com/example/tmdbapp/presentation/home/MovieTabComponent.kt
@@ -1,9 +1,10 @@
 package com.example.tmdbapp.presentation.home
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.TabRow
 import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
@@ -11,51 +12,63 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color.Companion.LightGray
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.material.Tab
 import androidx.compose.material.Text
+import androidx.compose.ui.graphics.Color.Companion.Gray
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.tmdbapp.R
+import com.example.tmdbapp.core.utils.Constants
+import com.example.tmdbapp.core.utils.toTabName
 import com.example.tmdbapp.domain.model.Movie
-import com.example.tmdbapp.ui.theme.TMDBAppTheme
 
 @Composable
 fun MovieTabComponent(
     selectedTabIndex: Int,
-    onSelectedTab: (TabPage) -> Unit,
+    onSelectedTab: (Int, Constants.Tabs) -> Unit,
+    movies: List<Movie>
 ){
-    TabRow(selectedTabIndex = selectedTabIndex,
-        indicator = {tabPositions ->
-            Box(
-                modifier = Modifier
-                    .tabIndicatorOffset(tabPositions[selectedTabIndex])
-                    .height(4.dp)
-                    .padding(horizontal = 28.dp)
-                    .clip(RoundedCornerShape(8.dp))
-                    .background(color = LightGray)
-            )
-        }
-        ) {
-            TabPage.values().forEachIndexed { index, tabs ->
-                Tab(
-                    selected = index == selectedTabIndex,
-                    onClick = {onSelectedTab(tabs)},
-                    text = {Text(text = tabs.name, maxLines = 1, fontSize = 14.sp)}
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        TabRow(selectedTabIndex = selectedTabIndex,
+            indicator = {tabPositions ->
+                Box(
+                    modifier = Modifier
+                        .tabIndicatorOffset(tabPositions[selectedTabIndex])
+                        .height(4.dp)
+                        .padding(horizontal = 28.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(color = LightGray)
                 )
             }
-    }
-}
-enum class TabPage{
-    NOW_PLAYING,
-    UPCOMING,
-    TOP_RATED,
-    POPULAR
-}
-
-@Preview
-@Composable
-fun MovieTabComponentPreview(){
-    TMDBAppTheme {
-        MovieTabComponent(selectedTabIndex = 1, onSelectedTab = {})
+        ) {
+            Constants.Tabs.values().forEachIndexed { index, tabs ->
+                Tab(
+                    modifier = Modifier
+                        .background(Gray),
+                    selected = index == selectedTabIndex,
+                    onClick = {onSelectedTab(index,tabs)},
+                    text = {Text(text = tabs.name.toTabName(), maxLines = 1, fontSize = 14.sp)}
+                )
+            }
+        }
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            verticalArrangement = Arrangement.spacedBy(18.dp)
+        ) {
+            items(movies) {item: Movie ->
+                AsyncImage(
+                    modifier = Modifier
+                        .height(146.dp)
+                        .width(100.dp),
+                    model = stringResource(id = R.string.poster_template, item.poster),
+                    contentDescription = null
+                )
+            }
+        }
     }
 }
