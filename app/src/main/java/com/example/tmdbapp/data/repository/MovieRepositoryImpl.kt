8b05394e1b1fe7bded143bba5b5d431136daec79
ToskePG <danilo.tosic.pmf@gmail.com>
2023-05-07 19:27:21 +0200

    Overrides functions from interface for API calls

--- app/src/main/java/com/example/tmdbapp/data/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/example/tmdbapp/data/repository/MovieRepositoryImpl.kt
@@ -1,12 +1,32 @@
 package com.example.tmdbapp.data.repository
 
+import com.example.tmdbapp.core.utils.NetworkResponse
 import com.example.tmdbapp.data.remote.services.MovieApi
+import com.example.tmdbapp.domain.model.Movie
 import com.example.tmdbapp.domain.repository.MovieRepository
+import kotlinx.coroutines.flow.Flow
 
 class MovieRepositoryImpl(
     private val api: MovieApi
 ) : MovieRepository {
-    override suspend fun getMovies() {
+    override suspend fun getPopularMovies(): Flow<NetworkResponse<List<Movie>>> {
         TODO("Not yet implemented")
     }
+
+    override suspend fun getNowPlayingMovies(): Flow<NetworkResponse<List<Movie>>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getUpcomingMovies(): Flow<NetworkResponse<List<Movie>>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTopRatedMovies(): Flow<NetworkResponse<List<Movie>>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getSearch(query: String): Flow<NetworkResponse<List<Movie>>> {
+        TODO("Not yet implemented")
+    }
+
 }
