2bfc5e3b2f7cf59aa0db437acabdd6659870fed4
ToskePG <danilo.tosic.pmf@gmail.com>
2023-05-08 00:05:47 +0200

    Adds search screen to navigation

--- app/src/main/java/com/example/tmdbapp/core/navigation/destinations/SearchScreen.kt
+++ app/src/main/java/com/example/tmdbapp/core/navigation/destinations/SearchScreen.kt
@@ -1,11 +1,26 @@
 package com.example.tmdbapp.core.navigation.destinations
 
+import androidx.compose.runtime.remember
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.example.tmdbapp.core.navigation.Routes
 import com.example.tmdbapp.core.navigation.Routes.SEARCH_SCREEN
+import com.example.tmdbapp.presentation.SharedViewModel
+import com.example.tmdbapp.presentation.search.SearchScreen
 
-fun NavGraphBuilder.searchScreenComposable(){
-    composable(route = SEARCH_SCREEN) {
-        //TODO add a real screen
+fun NavGraphBuilder.searchScreenComposable(
+    navController: NavController
+){
+    composable(route = SEARCH_SCREEN) { navBackStackEntry ->
+        val parentEntry = remember(navBackStackEntry) {
+            navController.getBackStackEntry(Routes.ROOT)
+        }
+        val sharedViewModel = hiltViewModel<SharedViewModel>(parentEntry)
+        SearchScreen(
+            viewModel = sharedViewModel,
+            onArrowClicked = {navController.popBackStack()}
+        )
     }
 }
